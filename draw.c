6dbf6e3182a4f9bac1673ddd2dd3dbfc11297012
arvind <amahankali@stuy.edu>
2017-02-16 02:23:40 -0500

    fin

--- draw.c
+++ draw.c
@@ -8,7 +8,7 @@
 
 //switched - true if octant 2 or octant 7
 //downward - true if octant 7 or octant 8
-void draw_line_Octant1(int x0, int y0, int x1, int y1, screen s, color c, int switched, int downward) {
+void draw_line_oct(int x0, int y0, int x1, int y1, screen s, color c, int switched, int downward) {
 
 	int A = (y1 - y0);
 	int B = -1 * (x1 - x0);
@@ -16,8 +16,8 @@ void draw_line_Octant1(int x0, int y0, int x1, int y1, screen s, color c, int sw
 
 	int x = x0; int y = y0;
 
-	printf("Switched: %d\n", switched);
-	printf("Downward: %d\n", downward);
+	//printf("Switched: %d\n", switched);
+	//printf("Downward: %d\n", downward);
 
 	while(x <= x1)
 	{
@@ -75,14 +75,13 @@ void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
 		y0 = y1;
 		y1 = temp;
 	}
-
-	printf("(x0: %d, y0: %d, x1: %d, y1: %d)\n", x0, y0, x1, y1);
+	printf("(%d, %d) --> (%d, %d)\n", x0, y0, x1, y1);
 
 	int deltaX = (x1 - x0);
 	int deltaY = (y1 - y0);
 
-	if(deltaY > deltaX) draw_line_Octant1(y0, x0, y1, x1, s, c, 1, 0);
-	else if(deltaY <= deltaX && deltaY >= 0) draw_line_Octant1(x0, y0, x1, y1, s, c, 0, 0);
-	else if(deltaY < 0 && deltaY >= -1 * deltaX) draw_line_Octant1(x0, y0, x1, 2 * y0 - y1, s, c, 0, 1);
-	else draw_line_Octant1(y0, x0, 2 * y0 - y1, x1, s, c, 1, 1);
+	if(deltaY > deltaX) draw_line_oct(y0, x0, y1, x1, s, c, 1, 0);
+	else if(deltaY <= deltaX && deltaY >= 0) draw_line_oct(x0, y0, x1, y1, s, c, 0, 0);
+	else if(deltaY < 0 && deltaY >= -1 * deltaX) draw_line_oct(x0, y0, x1, 2 * y0 - y1, s, c, 0, 1);
+	else draw_line_oct(y0, x0, 2 * y0 - y1, x1, s, c, 1, 1);
 }
